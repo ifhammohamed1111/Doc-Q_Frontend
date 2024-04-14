9cd00a2835755d2a17d7f9fef58d45fc003e21f8
iambharatchavhan <bharatchavhan141@gmail.com>
2024-04-14 19:29:12 +0530

    [UPDATE] header added in service page

--- Services_Panel/javascript/app.js
+++ Services_Panel/javascript/app.js
@@ -62,30 +62,6 @@ document.querySelectorAll('.slide-images img').forEach(image => {
 
 
 
-var toggle=false;
-var div = document.getElementById("setIcons");
-
-
- 
-
-const handleMenu = ()=>{
-    console.log("ho")
- 
-    if(toggle===true){
-        div.style.display="block";
-         
-        div.style.alignItems="center"; 
-       
-        toggle=false;
-    }else{
-        div.style.display="none";
-        console.log(div)
-        toggle=true;
-    } 
-}
-
-
-
 
 
 
