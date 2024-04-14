0952d53c839204b69d7554d861f429e7fb555861
vasanshettiTharun <vasanshettitharun@gmail.com>
2024-04-14 20:25:30 +0530

    doctor_profile_booking_functionality

--- Doctors_Profile/script.js
+++ Doctors_Profile/script.js
@@ -21,8 +21,44 @@ const handleMenu = ()=>{
 }
 
 
+// time slots js
+let selectedSlot = null;
 
+    document.querySelectorAll('.time-slot').forEach(slot => {
+        slot.addEventListener('click', () => {
+            // Remove previous selection
+            if (selectedSlot) {
+                selectedSlot.classList.remove('selected');
+            }
+            // Highlight the selected slot
+            slot.classList.add('selected');
+            selectedSlot = slot;
 
+            document.querySelectorAll('.time-slot').forEach(otherSlot => {
+                if (otherSlot !== slot) {
+                    otherSlot.style.opacity = '0.5';
+                }
+                if (otherSlot == slot) {
+                    otherSlot.style.opacity = '10';
+                }
+
+                
+            });
+        });
+    });
+
+    document.getElementById('book-btn').addEventListener('click', () => {
+        if (selectedSlot) {
+            if (selectedSlot.dataset.booked) {
+                alert('This slot is already booked!');
+            } else {
+                alert('Booking successful!');
+                selectedSlot.dataset.booked = true;
+            }
+        } else {
+            alert('Please select a time slot before booking.');
+        }
+    });
 
 
 
